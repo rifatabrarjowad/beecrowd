871f8858f09c8033d96c53227c3f37985b4cf3e0
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-02-04 19:33:09 +0600

    update file

---
+++ ProbNumber02/rifta/rifta/Program.cs
@@ -0,0 +1,6 @@
+﻿var a = Console.ReadLine();
+var b = Console.ReadLine();
+int c = Convert.ToInt32(a);
+int d = Convert.ToInt32(b);
+var X = c + d;
+Console.WriteLine("X = " + X);
---
+++ ProbNumber03/ProbNumber03/Program.cs
@@ -0,0 +1,7 @@
+﻿var a = Console.ReadLine();
+
+Double b = Convert.ToDouble(a);
+double c = 3.14159;
+
+var que = c * (b * b);
+Console.WriteLine("A=" + que.ToString("0.0000"));
